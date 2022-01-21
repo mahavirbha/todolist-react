5bd0f1b92d441059972089cf0130772f1ccdf19d
Mahavirbha <robowithsomeaix@gmail.com>
2022-01-22 00:20:18 +0530

    Work Started

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <>
+            <div className="header text-center" >
+                <h3>Todo List</h3>
+                <button className="btn btn-primary mt-2">Create Task</button>
+            </div>
+            <div className="task-container">
+            </div>
+        </>
+
+    );
+};
+
+export default TodoList;
